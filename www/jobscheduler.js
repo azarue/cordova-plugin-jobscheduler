84ee364232e962c62031f0a3f9b576e9f6166f24
Errietta Kostala <errietta@errietta.me>
2017-04-23 00:03:30 +0100

    Make it so each job has a callback attached to it

--- www/jobscheduler.js
+++ www/jobscheduler.js
@@ -2,11 +2,22 @@ var exec    = require('cordova/exec'),
     channel = require('cordova/channel');
 
 exports._jobid = 0;
+exports._callbacks = {};
 
-exports.onrun = function() {
-}
+exports.onrun = function(jobid) {
+    var cb;
+
+    if (cb = this._callbacks[jobid]) {
+        cb();
+    }
+};
 
 exports.schedule = function(args) {
+    var callback = args.callback;
+    delete args.callback;
+
+    this._callbacks[this._jobid] = callback;
+
     args.jobId = this._jobid++;
     cordova.exec(function() { console.log('schedule called'); }, null, 'JobScheduler', 'schedule', [args]);
 };
